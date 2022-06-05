e1828a53e1b526d181a30e3a225f85f2b6dd05ff
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-06-04 22:45:16 -0300

    training agent with 20 epochs

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # Q-learning settings
 learning_rate = 0.0001
 discount_factor = 0.99
-train_epochs = 5
+train_epochs = 20
 learning_steps_per_epoch = 2000
 replay_memory_size = 10000
 
@@ -9,16 +9,16 @@ replay_memory_size = 10000
 batch_size = 64
 
 # Training regime
-test_episodes_per_epoch = 10
+test_episodes_per_epoch = 5
 
 # Other parameters
 frame_repeat = 12
 resolution = (30, 45)
-episodes_to_watch = 10
-model_savefile = "doom-agent.pth"
-save_model = False
-load_model = True
-skip_learning = True
+episodes_to_watch = 5
+model_savefile = "doom-agent-20-epochs.pth"
+save_model = True
+load_model = False
+skip_learning = False
 visible_during_train = False
 doom_red_color = [0, 0, 203]
 doom_blue_color = [203, 0, 0]
